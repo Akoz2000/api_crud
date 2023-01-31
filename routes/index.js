4bf12a9f9ab5aff48fadec2131f11b73b18a6f4c
Akoz2000 <87030833+Akoz2000@users.noreply.github.com>
2023-01-31 23:54:37 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@ const ObjectId = require('mongoose').Types.ObjectId;
 
 const { Employee }= require('../models/employee');
 
+
+
+
+
+
 router.get('/api/employees', (req, res) => {
     Employee.find({}, (err, data) => {
         if(!err) {
@@ -15,13 +20,27 @@ router.get('/api/employees', (req, res) => {
 });
 
 router.get('/api/employee/:id', (req, res) => {
-    Employee.findById(req.params.id, (err, data) => {
-        if(!err) {
-            res.send(data);
-        } else {
-           console.log(err);
-        }
-    });
+    //var query  = Employee.where({ employeeId: req.params.employeeId }); 
+    //Employee.findById(req.params.id, (err, data) => {
+    //query.findOne(function (err, employee){  
+        //let num = 15;
+        //let text = num.toString();
+    Employee.findOne({ employeeId: parseInt(req.params.id) }, function(err, data) {
+    if(!err) {
+             res.send(data);
+         } else {
+            console.log(err);
+         }
+     });
+     
+
+    // var query  = Employee.where({ _id: req.params.employeeId }); // <-- Use the correct param name
+    // query.findOne(function (err, employee) {
+    //     if (err)
+    //         return res.send(err)
+    //     res.json(employee);
+    //     });
+
 });
 
 router.put('/api/employee/update/:id', (req, res) => {
@@ -30,10 +49,12 @@ router.put('/api/employee/update/:id', (req, res) => {
     const emp = {
         name: req.body.name,
         email: req.body.email,
-        salary: req.body.salary
+       
+       // salary: req.body.salary
     };
-    Employee.findByIdAndUpdate(req.params.id, { $set: emp }, { new: true }, (err, data) => {
-        if(!err) {
+    Employee.findOneAndUpdate({ employeeId: parseInt(req.params.id) }, { $set: emp }, { new: true }, (err, data) => {
+        //Employee.findOne({ employeeId: parseInt(req.params.id) }, function(err, data) {  
+    if(!err) {
             res.status(200).json({code: 200, message: 'Employee Updated Successfully', updateEmployee: data})
         } else {
             console.log(err);
@@ -49,7 +70,7 @@ router.post('/api/employee/add', (req, res) => {
     const emp = new Employee({
         name: req.body.name,
         email: req.body.email,
-        salary: req.body.salary
+        //salary: req.body.salary
     });
     emp.save((err, data) => {
         if(!err) {
@@ -63,7 +84,8 @@ router.post('/api/employee/add', (req, res) => {
 
 router.delete('/api/employee/:id', (req, res) => {
 
-    Employee.findByIdAndRemove(req.params.id, (err, data) => {
+    //Employee.findByIdAndRemove(req.params.id, (err, data) => {
+        Employee.findOneAndRemove({ employeeId: parseInt(req.params.id) }, function(err, data) {
         if(!err) {
             // res.send(data);
             res.status(200).json({code: 200, message: 'Employee deleted', deleteEmployee: data})
